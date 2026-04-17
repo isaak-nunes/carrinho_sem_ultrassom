d1926bbc590ff8594067057b6adb174c0c6b3edb
isaak-nunes <isaakenrick@usp.br>
2026-04-17 15:49:07 -0300

    carrinho_sem_ultrassom

--- src/main.c
+++ src/main.c
@@ -1,4 +1,10 @@
-//versão 4.2
+/*versão 4.2
+versão do carrinho que:
+-caso ambos os sensores deem BRANCO, ele siga em frente e o led liga amarelo
+-caso um dos sensores deem preto, ele curve para um lado e o led liga,
+ ou vermelho (com curva pra direita), ou verde (com curva pra esquerda)
+-caso ambos os sensores deem preto, ele siga em frente e o led NÃO liga
+*/
 //:)
 
 #include <zephyr/kernel.h>             // Funções básicas do Zephyr (ex: k_msleep, k_thread, etc.)
@@ -6,75 +12,121 @@
 #include <zephyr/drivers/gpio.h>       // API para controle de pinos de entrada/saída (GPIO)
 #include <pwm_z42.h>   
 
-#define INPUT_PORT      DT_NODELABEL(gpioa)  // Porta E = GPIO_4 no seu .dts
-#define INPUT_PIN       1         // PTE20
-#define INPUT_PORT2     DT_NODELABEL(gpioa)  // Porta E = GPIO_4 no seu .dts
-#define INPUT_PIN2      2         // PTE20
-#define max 750
-#define min 0
-#define start 1000
+#define INPUT_PORT      DT_NODELABEL(gpioa)  // Porta A = GPIO_0 no seu .dts
+#define INPUT_PIN       1         // PTA1
+#define INPUT_PIN2      2         // PTA2
+#define max 1200
+#define min 300
+
 
 #define TPM_MODULE 1000         // Define a frequência do PWM fpwm = (TPM_CLK / (TPM_MODULE * PS))
 
-void main (void)
+void mtr_Vrd_f(void)
+{   
+    //função pro motor verde ir pra frente
+    pwm_tpm_CnV(TPM0, 2, max);
+    pwm_tpm_CnV(TPM1, 0, max);
+}
+
+//função pro motor verde ir pra trás
+void mtr_Vrd_t(void)             
 {
+        pwm_tpm_CnV(TPM0, 2, min);
+        pwm_tpm_CnV(TPM1, 0, min);
+}
+
+void mtr_Vrm_f(void)             //função pro motor vermelho ir pra frente
+{
+        pwm_tpm_CnV(TPM0, 3, max);
+        pwm_tpm_CnV(TPM1, 1, max);
+}
+
+void mtr_Vrm_t(void)             //função pro motor vermelho ir pra trás
+{
+        pwm_tpm_CnV(TPM0, 3, min);
+        pwm_tpm_CnV(TPM1, 1, min);
+}
+
+int main (void)
+{
+    //isso aqui faz algo importante 
     const struct device *input_dev;
-    const struct device *input_dev2;
-    
-    int ret, val, ret2, val2;
-    pwm_tpm_Init(TPM2, TPM_PLLFLL, TPM_MODULE, TPM_CLK, PS_128, EDGE_PWM);
-    pwm_tpm_Init(TPM0, TPM_PLLFLL, TPM_MODULE, TPM_CLK, PS_128, EDGE_PWM);
-
     input_dev = DEVICE_DT_GET(INPUT_PORT);
-    input_dev2 = DEVICE_DT_GET(INPUT_PORT2);
+    int infra_vrm, val, infra_vrd, val2;
 
-    ret = gpio_pin_configure(input_dev, INPUT_PIN, GPIO_INPUT);
-    ret2 = gpio_pin_configure(input_dev2, INPUT_PIN2, GPIO_INPUT);
+    //iniciação do clock da porta (eu acho)
+    pwm_tpm_Init(TPM0, TPM_PLLFLL, TPM_MODULE, TPM_CLK, PS_128, EDGE_PWM);
+    pwm_tpm_Init(TPM1, TPM_PLLFLL, TPM_MODULE, TPM_CLK, PS_128, EDGE_PWM);
+    pwm_tpm_Init(TPM2, TPM_PLLFLL, TPM_MODULE, TPM_CLK, PS_128, EDGE_PWM);
 
-        pwm_tpm_Ch_Init(TPM2, 0, TPM_PWM_H, GPIOB, 18);
-        pwm_tpm_Ch_Init(TPM2, 1, TPM_PWM_H, GPIOB, 19);
-        pwm_tpm_Ch_Init(TPM0, 1, TPM_PWM_H, GPIOD, 1);
+        //habilita o pino como input, dos sensores infravermelho e do ultrassom
+        infra_vrm = gpio_pin_configure(input_dev, INPUT_PIN, GPIO_INPUT);
+        infra_vrd = gpio_pin_configure(input_dev, INPUT_PIN2, GPIO_INPUT);
 
-        pwm_tpm_Ch_Init(TPM0, 2, TPM_PWM_H, GPIOD, 2);  //motor pra frente
-        pwm_tpm_Ch_Init(TPM0, 3, TPM_PWM_H, GPIOD, 3);  //motor pra frente
+        pwm_tpm_Ch_Init(TPM2, 0, TPM_PWM_H, GPIOB, 18); //led vermelho
+        pwm_tpm_Ch_Init(TPM2, 1, TPM_PWM_H, GPIOB, 19); //led verde
 
-        pwm_tpm_Ch_Init(TPM0, 0, TPM_PWM_H, GPIOD, 0);  //motor pra trás
-        pwm_tpm_Ch_Init(TPM0, 5, TPM_PWM_H, GPIOD, 5);  //motor pra trás
+        //motores pra frente
+        pwm_tpm_Ch_Init(TPM0, 2, TPM_PWM_H, GPIOD, 2);
+        pwm_tpm_Ch_Init(TPM0, 3, TPM_PWM_H, GPIOD, 3); 
+
+        //motores pra trás
+        pwm_tpm_Ch_Init(TPM1, 0, TPM_PWM_L, GPIOA, 12);  
+        pwm_tpm_Ch_Init(TPM1, 1, TPM_PWM_L, GPIOA, 13);  
 
     while (1) {
         val = gpio_pin_get(input_dev, INPUT_PIN);
-        val2 = gpio_pin_get(input_dev2, INPUT_PIN2);
+        val2 = gpio_pin_get(input_dev, INPUT_PIN2);
 
-        //vermelho
-        {if (val == 0)
+        //vermelho, vulgo direita
+        if (val == 0)
         {
             pwm_tpm_CnV(TPM2, 0, 0);        //led
-            pwm_tpm_CnV(TPM0, 2, max);
-            pwm_tpm_CnV(TPM0, 0, min);
+
+            mtr_Vrd_f();
 
         }
         else
         {
             pwm_tpm_CnV(TPM2, 0, 1000);
-            pwm_tpm_CnV(TPM0, 2, min);
-            pwm_tpm_CnV(TPM0, 0, max);
+
+            /*esse if existe para que caso ambas
+            as variaveis resultem em um, o carrinho
+            contiue a seguir em frente*/
+            if (val2 != 1)
+            {
+                mtr_Vrd_t();
+            }
+            else
+            {
+                 mtr_Vrd_f();
+            }
         }
-    }
+    
         
-        //verde
-        {if (val2 != 1)
+        //verde, vulgo esquerda
+        if (val2 == 0)
         {
             pwm_tpm_CnV(TPM2, 1, 0);        //led
-            pwm_tpm_CnV(TPM0, 3, max);
-            pwm_tpm_CnV(TPM0, 5, min);
+
+            mtr_Vrm_f();
         }
         else
         {
             pwm_tpm_CnV(TPM2, 1, 1000);
-            pwm_tpm_CnV(TPM0, 3, min);
-            pwm_tpm_CnV(TPM0, 5, max);
+            
+            /*esse if existe para que caso ambas
+            as variaveis resultem em um, o carrinho
+            contiue a seguir em frente*/
+            if (val != 1)
+            {
+                mtr_Vrm_t();
+            }
+            else{
+                 mtr_Vrm_f();
+            }
         }
+   
     }
     k_msleep(30);
-    }
 }
