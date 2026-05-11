d5a481c119c316f23a359106c723640488cf967f
isaak-nunes <isaakenrick@usp.br>
2026-05-11 14:06:22 -0300

    em processo de implementação de PID

--- src/main.c
+++ src/main.c
@@ -18,26 +18,33 @@ versão do carrinho que:
 #include <sensor-ultrassonico.h>       //biblioteca do ultrassom 
 
 #define INPUT_PORT      DT_NODELABEL(gpioa)  // Porta A = GPIO_0 no seu .dts
-#define INPUT_PIN       4        // PTA1, vulgo, porta do sensor vermalho (direita)
-#define INPUT_PIN2      5        // PTA2, vulgo, porta do sensor verde (esquerda)
-#define INPUT_PIN4      1        // PTA2, vulgo, porta do sensor do meio 
+#define INPUT_PIN       5        // PTA5, vulgo, porta do sensor verde (esquerda)
+#define INPUT_PIN2      4        // PTA4, vulgo, porta do sensor vermalho (direita)
+#define INPUT_PIN4      1        // PTA13, vulgo, porta do sensor do meio 
 
 #define tpm 1000                    // Define a frequência do PWM fpwm = (TPM_CLK / (tpm * PS))
 
-#define max tpm * 50 / 100       // Velocidade máxima proporcional ao tpm
-#define max tpm * 45 / 100          //velocidade maxima
-#define min tpm * 10 / 100          // Velocidade mínima proporcionao ao tpm
-#define paro 26                     // quantos cm pa para
+#define frntG   900
+#define trasG   200
+#define frntS   600
+#define trasS   400
 
+#define paro 0                     // quantos cm pa para
+
+
+volatile uint8_t err = 0;   // variavel relacionada a possiveis falhas do ultrassom do carrinho
 volatile uint8_t gus= 0;    // variavel relacionada ao sensor direito do carrinho  
 volatile uint8_t tavo= 0;   // variavel relacionada ao sensor esquerdo do carrinho
 volatile uint8_t reh= 0;    // variavel relacionada ao sensor central do carrinho
-volatile uint8_t err = 0;   // variavel relacionada a possiveis falhas do ultrassom do carrinho
-volatile uint8_t dir = 0;   //
-volatile uint8_t esq = 0;   //
 
-void mtr_Vrd_f(int v)       //função pro motor verde (esquerdo) ir pra frente      
-{   
+// variaveis para o controle PID
+volatile uint8_t Kp= 35;    // peso da proporção
+volatile uint8_t Ki= 0;    // peso da integral
+volatile uint8_t Kd= 35;    // peso da derivada
+
+volatile uint8_t P = 0, I = 0, D = 0, PID = 0;    // -\(^-^)/-
+
+void mtr_Vrd_f(int v){       //função pro motor verde (esquerdo) ir pra frente      
     pwm_tpm_CnV(TPM0, 2, v);     //Quanto MAIOR, mais rápido
     pwm_tpm_CnV(TPM0, 0, tpm);          //Quanto MAIOR, menos rápido
 }
@@ -50,7 +57,7 @@ void mtr_Vrd_t(int d)        //função pro motor verde (esquerdo) ir pra trás
 
 void mtr_Vrm_f(int v)       //função pro motor vermelho (direito) ir pra frente   
 {
-    pwm_tpm_CnV(TPM0, 3, v + 100);      //Quanto MAIOR, mais rápido
+    pwm_tpm_CnV(TPM0, 3, v);      //Quanto MAIOR, mais rápido
     pwm_tpm_CnV(TPM0, 4, tpm);      //Quanto MAIOR, menos rápido
 }
 
@@ -77,8 +84,9 @@ void setup(void) { //pois quanto menor o main, melhor!
     pwm_tpm_Init(TPM0, TPM_PLLFLL, tpm, TPM_CLK, PS_128, EDGE_PWM);
     pwm_tpm_Init(TPM2, TPM_PLLFLL, tpm, TPM_CLK, PS_128, EDGE_PWM);
 
-        pwm_tpm_Ch_Init(TPM2, 0, TPM_PWM_H, GPIOB, 18); //led vermelho
-        pwm_tpm_Ch_Init(TPM2, 1, TPM_PWM_H, GPIOB, 19); //led verde
+        //pwm_tpm_Ch_Init(TPM2, 0, TPM_PWM_H, GPIOB, 18); //led vermelho
+        //pwm_tpm_Ch_Init(TPM2, 1, TPM_PWM_H, GPIOB, 19); //led verde
+        pwm_tpm_Ch_Init(TPM0, 1, TPM_PWM_H, GPIOD, 1);
 
         //motores pra frente
         pwm_tpm_Ch_Init(TPM0, 2, TPM_PWM_H, GPIOD, 2);  //lado direito (vermelho)
@@ -102,41 +110,50 @@ int main (void)
     gpio_pin_configure(input_dev, INPUT_PIN4, GPIO_INPUT);//central
 
     while (1) {
-        gus = gpio_pin_get(input_dev, INPUT_PIN);
+        gus  = gpio_pin_get(input_dev, INPUT_PIN);
         tavo = gpio_pin_get(input_dev, INPUT_PIN2);
-        reh = gpio_pin_get(input_dev, INPUT_PIN4);
+        reh = gpio_pin_get(input_dev, INPUT_PIN4);;
         float aux;
         //giro suave
         if (reh == 1){
+            pwm_tpm_CnV(TPM0, 1, 1000);
             //para esquerda
             if(gus == 1 && tavo == 0){
-                mtr_Vrd_f(max);
-                mtr_Vrm_t(min);
+                mtr_Vrd_f(frntS); //esquerda para frente
+                mtr_Vrm_t(trasS); //direita para atras
             }
             //para direita
-            if(gus == 0 && tavo == 1){
-                mtr_Vrd_t(min);
-                mtr_Vrm_f(max);
+            else if(gus == 0 && tavo == 1){
+                mtr_Vrd_t(trasS); //esquerda para atras
+                mtr_Vrm_f(frntS); //direita para frente
             }
+
+            //caso contrario, va para frente!
+        else{
+            mtr_Vrd_f(frntS);
+            mtr_Vrm_f(frntS);
+        }
         }
 
         //giro grosseiro
         else if (reh == 0){
+            pwm_tpm_CnV(TPM0, 1, 0);
             //para esquerda
             if(gus == 1 && tavo == 0){
-                mtr_Vrd_f(max);
-                mtr_Vrm_t(0);
+                mtr_Vrd_f(frntG); //esquerda para frente
+                mtr_Vrm_t(trasG);   //direita para atras
             }
             //para direita
-            if(gus == 0 && tavo == 1){
-                mtr_Vrd_t(0);
-                mtr_Vrm_f(max);
+            else if(gus == 0 && tavo == 1){
+                mtr_Vrd_t(trasG);   //esquerda para atras
+                mtr_Vrm_f(frntG); //direita para frente
             }
-        }
-        //caso contrario, va para frente!
+
+            //caso contrario, va para frente!
         else{
-            mtr_Vrd_f(tpm);
-            mtr_Vrm_f(tpm);
+            mtr_Vrd_f(frntS);
+            mtr_Vrm_f(frntS);
+        }
         }
         k_msleep(20);
         aux = distancia;
